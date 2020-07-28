1497e401e8dee04efef6d9909f4390caf41483b2
Kris <hoosyer@gmail.com>
2020-07-27 20:21:28 -0400

    Initial Project Creation

---
+++ QuestionGame/Program.cs
@@ -0,0 +1,27 @@
+﻿using QuestionLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var questions = Question.LoadQuestions();
+
+            foreach (var question  in questions)
+            {
+                Console.WriteLine(question.QuestionText);
+
+                // Need to display the question answer choices.
+
+                Console.Write("Your answer is? ");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ QuestionLibrary/Question.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionLibrary
+{
+    public class Question
+    {
+        public string QuestionText { get; set; }
+        public List<string> AnswerOptions { get; set; }
+
+        public static List<Question> LoadQuestions()
+        {
+            var questions = new List<Question>();
+
+            try
+            {
+                // Build a question and add it to the list.
+                questions.Add(new Question()
+                {
+                    QuestionText = "What is the capital of Indiana?",
+                    AnswerOptions = new List<string>() { "Seymour", "Columbus", "New Washington", "Fort Wayne", "Indianapolis" }
+                });
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            return questions;
+        }
+    }
+}
