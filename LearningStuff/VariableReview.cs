351b92ef18ec2b832de75dfc8112edbe8a23dc48
Kris <hoosyer@gmail.com>
2020-07-29 16:27:04 -0400

    Add some examples

---
+++ LearningStuff/VariableReview.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningStuff
+{
+    public class VariableReview
+    {
+        public void VariableInt()
+        {
+            var usingVarDefaultsToInt = 100;
+            int thisIsAnotherInt = 200;
+            long isAVeryLargeNumber = 99999999999999;
+
+            // These can all be added together.
+            var total = usingVarDefaultsToInt + thisIsAnotherInt + isAVeryLargeNumber;
+
+            Console.WriteLine($"Sum: {total}");
+        }
+
+        public void VariableString()
+        {
+            string testString = "This is a string";
+            var anotherTestString = "String defined as var.";
+            var stringAsNumber = "12345";
+
+            Console.WriteLine("The string values:");
+            Console.WriteLine($"testString: {testString}");
+            Console.WriteLine($"anotherTestString: {anotherTestString}");
+            Console.WriteLine($"stringAsNumber: {stringAsNumber}");
+        }
+    }
+}
